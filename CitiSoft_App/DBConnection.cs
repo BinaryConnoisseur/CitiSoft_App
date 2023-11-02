5a5b689405c444df8805dba300c69da7cfe747f9
arsal <arsal@DELLXPS>
2023-11-02 12:00:33 +0000

    Checkmate

---
+++ CitiSoft_App/DBConnection.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CitiSoft_App
+{
+    internal class DBConnection
+    {
+        private DBConnection instance;
+
+        private string DBConnectionString;
+
+        private DBConnection() 
+        {
+            
+        }
+    }
+}
